a023a58b4a909d6b1864a194cadd1481dfe26c6f
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2023-06-19 11:22:25 +0900

    refactor: add static import of Assertions.assertThat

--- src/test/java/persistence/sql/util/ColumnValueTest.java
+++ src/test/java/persistence/sql/util/ColumnValueTest.java
@@ -1,15 +1,16 @@
 package persistence.sql.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ColumnValueTest {
 
     @Test
     @DisplayName("String 이면 따옴표가 붙는다.")
     void stringType() {
-        Assertions.assertThat(
+        assertThat(
                 ColumnValue.build("Nickname")
         ).isEqualTo("'Nickname'");
     }
@@ -17,7 +18,7 @@ class ColumnValueTest {
     @Test
     @DisplayName("String 이 아니면 따옴표가 붙지 않는다.")
     void integerType() {
-        Assertions.assertThat(
+        assertThat(
                 ColumnValue.build(1)
         ).isEqualTo("1");
     }
